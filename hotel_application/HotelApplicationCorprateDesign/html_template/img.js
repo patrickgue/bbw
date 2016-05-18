3937f1c07ec4f3679b04e9a6e9032205bc3e9b8b
patrickgue <me@patrickguenthard.ch>
2016-05-18 17:12:07 +0200

    minor modification

--- hotel_application/HotelApplicationCorprateDesign/html_template/img.js
+++ hotel_application/HotelApplicationCorprateDesign/html_template/img.js
@@ -1,13 +1,11 @@
 (function() {
     var imgs = document.getElementsByClassName("img");
     for(var i = 0; i < imgs.length; i++){
-	img = imgs[i];
-	img.onclick = function(){
-	    
-	    if(img.classList.contains("open")) {
-		img.classList.remove("open");
+	imgs[i].onclick = function(){
+	    if(this.classList.contains("open")) {
+		this.classList.remove("open");
 	    } else {
-		img.classList.add("open");
+		this.classList.add("open");
 	    }
 	};
     }
