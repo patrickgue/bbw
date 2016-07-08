dffae8e5fc84c21230b96341d167b8a1a1d5c4bd
patrickgue <me@patrickguenthard.ch>
2016-07-08 08:11:10 +0200

    added pdf library

---
+++ hotel_application/hotel-application-jsf-web/src/java/ch/eggbacon/app/pdf/PDFCreator.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2016 Patrick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.eggbacon.app.pdf;
+
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+/**
+ *
+ * @author Patrick
+ */
+public class PDFCreator {
+    public static void drawTable(PDPage page, PDPageContentStream contentStream,
+                             float y, float margin,
+                             String[][] content) throws IOException {
+        final int rows = content.length;
+        final int cols = content[0].length;
+        final float rowHeight = 20f;
+        final float tableWidth = page.getCropBox().getWidth() - margin - margin;
+        final float tableHeight = rowHeight * rows;
+        final float colWidth = tableWidth/(float)cols;
+        final float cellMargin=5f;
+
+        //draw the rows
+        float nexty = y ;
+        for (int i = 0; i <= rows; i++) {
+            contentStream.drawLine(margin, nexty, margin+tableWidth, nexty);
+            nexty-= rowHeight;
+        }
+
+        //draw the columns
+        float nextx = margin;
+        for (int i = 0; i <= cols; i++) {
+            contentStream.drawLine(nextx, y, nextx, y-tableHeight);
+            nextx += colWidth;
+        }
+
+        //now add the text
+        contentStream.setFont( PDType1Font.HELVETICA_BOLD , 12 );
+
+        float textx = margin+cellMargin;
+        float texty = y-15;
+        for(int i = 0; i < content.length; i++){
+            for(int j = 0 ; j < content[i].length; j++){
+                String text = content[i][j];
+                contentStream.beginText();
+                contentStream.moveTextPositionByAmount(textx,texty);
+                contentStream.drawString(text);
+                contentStream.endText();
+                textx += colWidth;
+            }
+            texty-=rowHeight;
+            textx = margin+cellMargin;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        PDDocument doc = new PDDocument();
+        PDPage page = new PDPage();
+        doc.addPage( page );
+        PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+
+        String[][] content = {
+            {"Name"," Time "},
+            {"HTC","01:25"},
+            {"Samsung Tab2","05:30"}
+        } ;
+
+        drawTable(page, contentStream, 700, 100, content);
+
+        contentStream.close();
+        doc.save("E:\\test.pdf" );
+    } 
+}
