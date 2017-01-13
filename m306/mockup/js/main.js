4a6a93b0a2c44c5065f31beb7ee4d6cae101ca25
patrickgue <me@patrickguenthard.ch>
2017-01-13 07:24:38 +0100

    M306: worked on mockup

--- m306/mockup/js/main.js
+++ m306/mockup/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  $("header").load("pageelms/menu.html")
+  $("header").load("pageelms/menu.html");
+  $(".contact").load("pageelms/contact.html");
+  $(".hamsterMenu").on("click", function() {
+    var id = $(this).attr("data-id");
+    $(".hamsterMenuWrapper").hide();
+    $("[data-id='w"+id+"']").show();
+  });
+
+  $(".close").on("click", function() {
+    $(".hamsterWindow").hide();
+    $(".hamsterMenuWrapper").show();
+  });
 });
