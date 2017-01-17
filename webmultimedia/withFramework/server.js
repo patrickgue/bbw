98b9f804dd957159b979f873cf94baf49ca7988d
patrickgue <me@patrickguenthard.ch>
2017-01-17 21:37:43 +0100

    upload of framework works now

--- webmultimedia/withFramework/server.js
+++ webmultimedia/withFramework/server.js
@@ -1,24 +1,42 @@
 "use strict";
 
 const mimeTypes = {
-    ".html" : "text/html",
-    ".js" : "application/html",
-    ".css" : "text/css",
-    ".png" : "png/image",
-    ".jpg" : "jpeg/image",
-    ".jpeg" : "jpeg/image",
-    ".svg" : "application/xml+svg",
-    ".ico" : "image/x-icon"
+    ".html": "text/html",
+    ".js": "application/javascript",
+    ".css": "text/css",
+    ".png": "png/image",
+    ".jpg": "jpeg/image",
+    ".jpeg": "jpeg/image",
+    ".svg": "application/xml+svg",
+    ".ico": "image/x-icon"
 };
 
-const defaultFile =  "index.html";
+const uploadMimeTypes = {
+    "VIDEO": [
+        "video/webm",
+        "video/ogg",
+        "video/mp4"
+    ],
+    "IMAGE": [
+        "image/png",
+        "image/jpeg",
+        "image/gif",
+        "image/svg+xml"
+    ],
+    "AUDIO": [
+        "audio/x-flac",
+        "audio/ogg"
+    ]
+};
+
+const defaultFile = "index.html";
 
 
 function generateToken(length) {
     var tokenString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     var token = "";
-    for(var i = 0; i < length; i++) {
-	token += tokenString[Math.floor(Math.random() * tokenString.length)];
+    for (var i = 0; i < length; i++) {
+        token += tokenString[Math.floor(Math.random() * tokenString.length)];
     }
     return token;
 }
@@ -31,21 +49,24 @@ const os = require('os');
 const sha1 = require('sha1');
 const database = require('./services/dbservice.js');
 const multer = require('multer');
+
 const PATH_PREFIX = "/api/v0";
 
 const htmlFilesPath = "/html/";
 
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
+    destination: function(req, file, cb) {
+        cb(null, './html/data')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + "_" + file.originalname)
+    }
 })
 
-var upload = multer({ storage: storage })
+var upload = multer({
+    storage: storage
+}).single("file");
 
 
 // Util methods
@@ -61,45 +82,43 @@ app.use(bodyParser.urlencoded({
 
 app.use(function(req, res, next) {
 
-    if(req.originalUrl.indexOf("/api/") == -1) {
+    if (req.originalUrl.indexOf("/api/") == -1) {
 
-	var defaultF = "";
+        var defaultF = "";
 
-	if(req.originalUrl.charAt(req.originalUrl.length - 1) == "/") {
-	    defaultF = defaultFile;
-	}
-	else {
-	    defaultF = "";
-	}
+        if (req.originalUrl.charAt(req.originalUrl.length - 1) == "/") {
+            defaultF = defaultFile;
+        } else {
+            defaultF = "";
+        }
 
-	console.log( __dirname, htmlFilesPath, req.originalUrl,  defaultF);
+        console.log(__dirname, htmlFilesPath, req.originalUrl, defaultF);
 
-	fs.readFile( __dirname + htmlFilesPath + req.originalUrl + defaultF, function (err, data) {
-	    if (err) {
-		if(err.code === "ENOENT") {
-		    res.status(404);
-		    res.type("text/plain");
-		    res.end("File not found: '"+req.originalUrl + "'");
-		    console.log("err")
-		}
-		else {
-		    res.status(500);
-		    console.log(err);
-		    res.end(JSON.stringify( err));
-		}
-	    } else {
-		for(var mimeType in mimeTypes) {
-		    if(req.originalUrl.indexOf(mimeType) != -1) {
-			res.type(mimeTypes[mimeType]);
-			break;
-		    }
-		}
-		res.end(data);
-	    }
-	});
+        fs.readFile(__dirname + htmlFilesPath + req.originalUrl + defaultF, function(err, data) {
+            if (err) {
+                if (err.code === "ENOENT") {
+                    res.status(404);
+                    res.type("text/plain");
+                    res.end("File not found: '" + req.originalUrl + "'");
+                    console.log("err")
+                } else {
+                    res.status(500);
+                    console.log(err);
+                    res.end(JSON.stringify(err));
+                }
+            } else {
+                for (var mimeType in mimeTypes) {
+                    if (req.originalUrl.indexOf(mimeType) != -1) {
+                        res.type(mimeTypes[mimeType]);
+                        break;
+                    }
+                }
+                res.end(data);
+            }
+        });
     } else {
-	res.header("Content-Type", "application/json");
-	next();
+        res.header("Content-Type", "application/json");
+        next();
     }
 });
 
@@ -125,11 +144,12 @@ app.post(PATH_PREFIX + "/user/add", function(req, res) {
     var password = sha1(req.body.userPassword);
 
     database.run(
-	"INSERT INTO TMUL_USER (userName,userPassword) VALUES(?,?)",
-	[user, password],
-	function(data) {
-	    res.end(JSON.stringify({userId : data}));
-    });
+        "INSERT INTO TMUL_USER (userName,userPassword) VALUES(?,?)", [user, password],
+        function(data) {
+            res.end(JSON.stringify({
+                userId: data
+            }));
+        });
 });
 
 /*
@@ -161,39 +181,75 @@ app.post(PATH_PREFIX + "/user/login", function(req, res) {
     var password = sha1(plainPassword);
 
     database.select("SELECT * FROM TMUL_USER WHERE userName = \"" + userName + "\"", function(data) {
-	console.log("SELECT * FROM TMUL_USER WHERE userName = \"" + userName + "\"");
-	if(data.length > 0) {
-	    for(let user of data) {
-		if(user.userPassword == password) {
-		    res.end(JSON.stringify({userId : user.userId}));
-		}
-		else {
-		    res.status(500).end(JSON.stringify(new error("Wrong password")));
-		}
-	    }
-	}
-	else {
-	    res.status(500);
-	    res.end(JSON.stringify(new error("user not found")));
-	}
+        console.log("SELECT * FROM TMUL_USER WHERE userName = \"" + userName + "\"");
+        if (data.length > 0) {
+            for (let user of data) {
+                if (user.userPassword == password) {
+                    res.end(JSON.stringify({
+                        userId: user.userId
+                    }));
+                } else {
+                    res.status(500).end(JSON.stringify(new error("Wrong password")));
+                }
+            }
+        } else {
+            res.status(500);
+            res.end(JSON.stringify(new error("user not found")));
+        }
     });
 });
 
+app.post(PATH_PREFIX + '/upload', function(req, res, next) {
+    upload(req, res, function(err) {
+        if (err) {
+            console.log('Error Occured');
+            return;
+        }
 
-app.post(PATH_PREFIX + '/upload', multer().array(), function (req, res, next) {
-  res.end("[]")
+        var fileType;
+
+
+        for (var uploadMimeType in uploadMimeTypes) {
+            if (uploadMimeTypes[uploadMimeType].indexOf(req.file.mimetype) != -1) {
+                fileType = uploadMimeType;
+                break;
+            }
+        }
+
+
+
+        database.run("INSERT INTO TMUL_MEDIA (mediaType, mediaTitle, mediaDescription, mediaPicturePath, mediaResourcePath) VALUES("+
+          '"'+ fileType +'",' +
+          '"'+ req.body.title +'",' +
+          '"'+ req.body.description +'",' +
+          '"'+ req.body.image +'",' +
+          '"data/'+ req.file.filename + "\")");
+
+        console.log("INSERT INTO TMUL_MEDIA (mediaType, mediaTitle, mediaDescription, mediaPicturePath, mediaResourcePath) VALUES("+
+          '"'+ fileType +'",' +
+          '"'+ req.body.title +'",' +
+          '"'+ req.body.description +'",' +
+          '"'+ req.body.image +'",' +
+          '"data/'+ req.file.filename + "\")");
+
+        console.log(req.file, req.body);
+        res.type("text/html");
+        res.end("Uploaded File<script>window.onload = function() {location.href = '/';}</script>");
+        console.log('Photo Uploaded');
+    })
+
 })
 
 app.get(PATH_PREFIX + "/media/all", function(req, res) {
     database.select("SELECT * FROM TMUL_MEDIA", function(data) {
-	res.end(JSON.stringify(data));
+        res.end(JSON.stringify(data));
     });
 });
 
 
 app.get(PATH_PREFIX + "/token", function(req, res) {
     res.end(JSON.stringify({
-	token : generateToken(32)
+        token: generateToken(32)
     }));
 });
 
--- webmultimedia/withFramework/services/dbservice.js
+++ webmultimedia/withFramework/services/dbservice.js
@@ -4,71 +4,64 @@ var file = "database.db";
 var exists = fs.existsSync(file);
 var db = new sqlite3.Database(file);
 
-db.serialize(function(){
-    if(!exists) {
+db.serialize(function() {
+    if (!exists) {
 
-      /* tables */
-	db.run("CREATE TABLE TMUL_USER ("
-               + "userId INTEGER PRIMARY KEY AUTOINCREMENT, "
-               + "userName STRING, "
-               + "userPassword STRING"
-               + ")");
-	db.run("CREATE TABLE TMUL_MEDIA ("
-	       + "mediaId INTEGER PRIMARY KEY AUTOINCREMENT, "
-	       + "mediaType STRING, "
-	       + "mediaTitle STRING,"
-	       + "mediaDescription STRING,"
-	       + "mediaPicturePath STRING, "
-	       + "mediaResourcePath STRING,"
-	       + "mediaUserId INTEGER"
-	       + ");");
+        /* tables */
+        db.run("CREATE TABLE TMUL_USER (" +
+            "userId INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "userName STRING, " +
+            "userPassword STRING" +
+            ")");
+        db.run("CREATE TABLE TMUL_MEDIA (" +
+            "mediaId INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "mediaType STRING, " +
+            "mediaTitle STRING," +
+            "mediaDescription STRING," +
+            "mediaPicturePath STRING, " +
+            "mediaResourcePath STRING," +
+            "mediaUserId INTEGER" +
+            ");");
 
-	db.run("INSERT INTO TMUL_USER (userId, userName, userPassword) "
-	       + "VALUES(1, \"pat\", \"a9993e364706816aba3e25717850c26c9cd0d89d\")");
+        db.run("INSERT INTO TMUL_USER (userId, userName, userPassword) " +
+            "VALUES(1, \"pat\", \"a9993e364706816aba3e25717850c26c9cd0d89d\")");
 
-	db.run("INSERT INTO TMUL_MEDIA (mediaId, mediaType, mediaTitle, mediaDescription, "
-	       + "mediaPicturePath, mediaResourcePath, mediaUserId)"
-	       + " VALUES (1, \"IMAGE\", \"Alfa Romeo\", \"Alfa Romeo Automobiles S.p.A. is an Italian car manufacturer. Founded as A.L.F.A., translating to Anonymous Lombard Automobile Factory in English) on June 24, 1910, in Milan, the company has been involved in car racing since 1911.\", \"data/WP_20160804_07_44_49_Rich.jpg\", \"data/WP_20160804_07_44_49_Rich.jpg\", 1)");
+        db.run("INSERT INTO TMUL_MEDIA (mediaId, mediaType, mediaTitle, mediaDescription, " +
+            "mediaPicturePath, mediaResourcePath, mediaUserId)" +
+            " VALUES (1, \"IMAGE\", \"Alfa Romeo\", \"Alfa Romeo Automobiles S.p.A. is an Italian car manufacturer. Founded as A.L.F.A., translating to Anonymous Lombard Automobile Factory in English) on June 24, 1910, in Milan, the company has been involved in car racing since 1911.\", \"data/WP_20160804_07_44_49_Rich.jpg\", \"data/WP_20160804_07_44_49_Rich.jpg\", 1)");
 
-	db.run("INSERT INTO TMUL_MEDIA (mediaId, mediaType, mediaTitle, mediaDescription, "
-	       + "mediaPicturePath, mediaResourcePath, mediaUserId)"
-	       + " VALUES (2, \"AUDIO\", \"Dance Song 97\", \"Dance Song 97 by Sleater Kinney on the Album Dig Me Out (1997)\", \"data/sleaterkinney-digmeout-1425.jpg\", \"data/sp1105-12_dance_song_97.mp3\", 1)");
+        db.run("INSERT INTO TMUL_MEDIA (mediaId, mediaType, mediaTitle, mediaDescription, " +
+            "mediaPicturePath, mediaResourcePath, mediaUserId)" +
+            " VALUES (2, \"AUDIO\", \"Dance Song 97\", \"Dance Song 97 by Sleater Kinney on the Album Dig Me Out (1997)\", \"data/sleaterkinney-digmeout-1425.jpg\", \"data/sp1105-12_dance_song_97.mp3\", 1)");
 
-
-	
-
-	       
-	    
-	       
-	
-      
-    }});
+    }
+});
 
 
 function select(query, done) {
-  var rows = [];
-  db.each(query, function(err,row) {
-    rows.push(row);
-  }, function() {
-    if(done !== undefined) {
-      done(rows);
-    }
-  });
+    var rows = [];
+    db.each(query, function(err, row) {
+        rows.push(row);
+    }, function() {
+        if (done !== undefined) {
+            done(rows);
+        }
+    });
 }
 
 function run(query, argsarray, done) {
-  var stmt = db.prepare(query);
-  stmt.run(argsarray);
-  stmt.finalize();
-  select("SELECT last_insert_rowid();", function(d) {
-    if(done !== undefined){
-	done(d[0]["last_insert_rowid()"]);
-    }
-  });
+    var stmt = db.prepare(query);
+    stmt.run(argsarray);
+    stmt.finalize();
+    select("SELECT last_insert_rowid();", function(d) {
+        if (done !== undefined) {
+            done(d[0]["last_insert_rowid()"]);
+        }
+    });
 
 }
 
 module.exports = {
-  select : select,
-  run : run
+    select: select,
+    run: run
 }
